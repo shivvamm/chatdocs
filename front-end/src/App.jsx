e8c9bdc5a47aa2f05696a2e3b5250e789fc9dfc0
shivvamm <shivam574488@gmail.com>
2024-08-04 22:58:52 +0530

    refactor: convert frontend to react

---
+++ front-end/src/App.jsx
@@ -0,0 +1,201 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+function App() {
+  const inputs = document.querySelectorAll(".input");
+  const [formData, setFormData] = useState({
+    email: "",
+    company_name: "",
+    chatbot_name: "",
+    base_link: "",
+    deployment_link: "",
+  });
+
+  const [focus, setFocus] = useState({
+    email: false,
+    company_name: false,
+    chatbot_name: false,
+    base_link: false,
+    deployment_link: false,
+  });
+
+  function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+  }
+
+  function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value == "") {
+      parent.classList.remove("focus");
+    }
+  }
+
+  inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("http://REDACTED_SERVER_IP/init_company/", JSON.stringify(formData), {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        console.log("Success response:", response.data);
+        setFormData({
+          email: "",
+          company_name: "",
+          chatbot_name: "",
+          base_link: "",
+          deployment_link: "",
+        });
+      })
+      .catch((error) => {
+        console.error("Error submitting to FastAPI:", error);
+      });
+  };
+
+  return (
+    <div className="App">
+      <div className="overlay" id="overlay"></div>
+      <div className="logo-container">
+        <img
+          src="logo.png"
+          alt="Jellyfish Technologies Logo"
+          className="logo"
+        />
+      </div>
+      <div className="container">
+        <span className="big-circle"></span>
+        <img src="img/shape.png" className="square" alt="" />
+        <div className="form">
+          <div className="contact-info">
+            <h3 className="title">Let's get in touch</h3>
+            <p className="text">
+              Jellyfish Technologies is your go-to partner for innovative
+              chatbot solutions and custom software development. With over 13
+              years of experience and 4000+ successful projects, we empower
+              businesses with cutting-edge technology.
+            </p>
+
+            <div className="info">
+              <div className="information">
+                <i className="fas fa-map-marker-alt"></i> &nbsp; &nbsp;
+                <p>
+                  D5 3rd floor, Logix Infotech Park, D Block, Sector 59, Noida,
+                  Uttar Pradesh 201301
+                </p>
+              </div>
+              <div className="information">
+                <i className="fas fa-envelope"></i> &nbsp; &nbsp;
+                <p>contact@jellyfishtechnologies.com</p>
+              </div>
+            </div>
+
+            <div className="social-media">
+              <p>Connect with us :</p>
+              <div className="social-icons">
+                <a href="https://www.facebook.com/REDACTED/?locale=hi_IN&_rdr">
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+                <a href="https://x.com/REDACTED?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
+                  <i className="fab fa-twitter"></i>
+                </a>
+                <a href="https://www.instagram.com/REDACTED/?hl=en">
+                  <i className="fab fa-instagram"></i>
+                </a>
+                <a href="https://www.linkedin.com/company/REDACTED">
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="contact-form">
+            <span className="circle one"></span>
+            <span className="circle two"></span>
+
+            <form id="contact-form" onSubmit={handleSubmit}>
+              <h3 className="title">Contact us</h3>
+              <div className="input-container">
+                <input
+                  type="email"
+                  name="email"
+                  className={`input ${focus.email ? "focus" : ""}`}
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+                <label htmlFor="email">Your Email</label>
+                <span>Enter Your Email</span>
+              </div>
+
+              <div className="input-container textarea">
+                <textarea
+                  name="company_name"
+                  className={`input ${focus.company_name ? "focus" : ""}`}
+                  value={formData.company_name}
+                  onChange={handleChange}
+                  required
+                ></textarea>
+                <label htmlFor="company_name">Company Name</label>
+                <span>Enter Your Company Name</span>
+              </div>
+              <div className="input-container">
+                <input
+                  type="text"
+                  name="chatbot_name"
+                  className={`input ${focus.chatbot_name ? "focus" : ""}`}
+                  value={formData.chatbot_name}
+                  onChange={handleChange}
+                  required
+                />
+                <label htmlFor="chatbot_name">Chatbot Name</label>
+                <span>Enter Your Chatbot Name</span>
+              </div>
+              <div className="input-container">
+                <input
+                  type="url"
+                  name="base_link"
+                  className={`input ${focus.base_link ? "focus" : ""}`}
+                  value={formData.base_link}
+                  onChange={handleChange}
+                  required
+                />
+                <label htmlFor="base_link">Source URL</label>
+                <span>Enter Your Source URL</span>
+              </div>
+              <div className="input-container">
+                <input
+                  type="text"
+                  name="deployment_link"
+                  className={`input ${focus.deployment_link ? "focus" : ""}`}
+                  value={formData.deployment_link}
+                  onChange={handleChange}
+                  required
+                />
+                <label htmlFor="deployment_link">Deployment Location</label>
+                <span>Enter Your Deployment Location</span>
+              </div>
+              <input type="submit" value="Send" className="btn" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
