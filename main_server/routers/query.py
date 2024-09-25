c2155881d24910887bea7044671c08ae3a6303e8
shivvamm <shivam574488@gmail.com>
2024-09-25 13:05:52 +0530

    fix: resolved issues

--- main_server/routers/query.py
+++ main_server/routers/query.py
@@ -39,105 +39,105 @@ prompt = ChatPromptTemplate.from_messages(
 
 @router.post("/query")
 async def answer_query(req: RequestModel, request: Request, db: db_dependency, user: dict = Depends(get_current_user)):
-    # try:
-    #     collection_name = user.company_key
-    #     chatbot_id = req.chatbot_id
-    #     session_id = req.session_id
-    #     logger.info("Chatbot ID: %s", req.chatbot_id)
-    #     logger.info("Session ID: %s", req.session_id)
-    #     logger.info("Company Key: %s", user.company_key)
+    try:
+        collection_name = user.company_key
+        chatbot_id = req.chatbot_id
+        session_id = req.session_id
+        logger.info("Chatbot ID: %s", req.chatbot_id)
+        logger.info("Session ID: %s", req.session_id)
+        logger.info("Company Key: %s", user.company_key)
 
-    #     logger.info("Request Headers: %s", request.headers)
+        logger.info("Request Headers: %s", request.headers)
 
-    #     origin_url = request.headers.get("origin")
-    #     if origin_url is None:
-    #         logger.error("Missing Origin Header")
-    #         raise HTTPException(status_code=400, detail="Missing Origin Header")
+        origin_url = request.headers.get("origin")
+        if origin_url is None:
+            logger.error("Missing Origin Header")
+            raise HTTPException(status_code=400, detail="Missing Origin Header")
 
-    #     chatbot_stats = db.query(Chatbot_stats).filter(Chatbot_stats.chatbot_id == req.chatbot_id).first()
-    #     if not chatbot_stats:
-    #         logger.error("Chatbot not found: %s", req.chatbot_id)
-    #         raise HTTPException(status_code=404, detail="Chatbot not found")
+        chatbot_stats = db.query(Chatbot_stats).filter(Chatbot_stats.chatbot_id == req.chatbot_id).first()
+        if not chatbot_stats:
+            logger.error("Chatbot not found: %s", req.chatbot_id)
+            raise HTTPException(status_code=404, detail="Chatbot not found")
 
-    #     if chatbot_stats.origin_url is None:
-    #         logger.error("Chatbot origin URL is None")
-    #         raise HTTPException(status_code=500, detail="Chatbot origin URL is None")
+        if chatbot_stats.origin_url is None:
+            logger.error("Chatbot origin URL is None")
+            raise HTTPException(status_code=500, detail="Chatbot origin URL is None")
 
-    #     logger.info("Chatbot Origin URL: %s", chatbot_stats.origin_url)
-    #     logger.info("Request Origin URL: %s", origin_url)
+        logger.info("Chatbot Origin URL: %s", chatbot_stats.origin_url)
+        logger.info("Request Origin URL: %s", origin_url)
 
-    #     if chatbot_stats.origin_url.strip() != origin_url.strip():
-    #         logger.warning("Unauthorized Domain: %s", origin_url)
-    #         raise HTTPException(status_code=401, detail="Unauthorized Domain")
+        if chatbot_stats.origin_url.strip() != origin_url.strip():
+            logger.warning("Unauthorized Domain: %s", origin_url)
+            raise HTTPException(status_code=401, detail="Unauthorized Domain")
         
-    #     rag_chain = prompt | llm | StrOutputParser()
-    #     with_message_history = RunnableWithMessageHistory(
-    #         rag_chain,
-    #         get_message_history,
-    #         input_messages_key="input",
-    #         history_messages_key="history",
-    #     )
+        rag_chain = prompt | llm | StrOutputParser()
+        with_message_history = RunnableWithMessageHistory(
+            rag_chain,
+            get_message_history,
+            input_messages_key="input",
+            history_messages_key="history",
+        )
 
-    #     final_response = await with_message_history.ainvoke(
-    #         {
-    #             "context": context_retriever(req.query, session_id, user.id, chatbot_id, db, collection_name),
-    #             "input": req.query,
-    #             "chatbot_name": chatbot_stats.chatbot_name,
-    #             "base_url": user.base_url,
-    #         },
-    #         config={"configurable": {"session_id": req.session_id}},
-    #     )
+        final_response = await with_message_history.ainvoke(
+            {
+                "context": context_retriever(req.query, session_id, user.id, chatbot_id, db, collection_name),
+                "input": req.query,
+                "chatbot_name": chatbot_stats.chatbot_name,
+                "base_url": user.base_url,
+            },
+            config={"configurable": {"session_id": req.session_id}},
+        )
 
-        # history = get_message_history(req.session_id)
-        # if history is None:
-        #     logger.error("Message history retrieval failed for session ID: %s", req.session_id)
-        #     raise HTTPException(status_code=500, detail="Message history retrieval failed")
+        history = get_message_history(req.session_id)
+        if history is None:
+            logger.error("Message history retrieval failed for session ID: %s", req.session_id)
+            raise HTTPException(status_code=500, detail="Message history retrieval failed")
 
-        # query_model = db.query(Queries).filter(
-        #     Queries.company_id == user.id,
-        #     Queries.chatbot_id == req.chatbot_id,
-        #     Queries.session_id == req.session_id,
-        #     Queries.query_text_user == req.query
-        # ).first()
+        query_model = db.query(Queries).filter(
+            Queries.company_id == user.id,
+            Queries.chatbot_id == req.chatbot_id,
+            Queries.session_id == req.session_id,
+            Queries.query_text_user == req.query
+        ).first()
 
-        # if query_model is None:
-        #     logger.error("Query not found for user ID: %s, chatbot ID: %s, session ID: %s", user.id, req.chatbot_id, req.session_id)
-        #     raise HTTPException(status_code=404, detail="Query not found")
+        if query_model is None:
+            logger.error("Query not found for user ID: %s, chatbot ID: %s, session ID: %s", user.id, req.chatbot_id, req.session_id)
+            raise HTTPException(status_code=404, detail="Query not found")
 
-        # context = query_model.query_context
-        # if context is None:
-        #     logger.error("Query context is None for session ID: %s", req.session_id)
-        #     raise HTTPException(status_code=500, detail="Query context is None")
+        context = query_model.query_context
+        if context is None:
+            logger.error("Query context is None for session ID: %s", req.session_id)
+            raise HTTPException(status_code=500, detail="Query context is None")
 
-        # combined_input = f"{context}\n{history}\n{req.query}"
-        # input_token = count_tokens(combined_input) + 10
-        # output_token = count_tokens(final_response)
+        combined_input = f"{context}\n{history}\n{req.query}"
+        input_token = count_tokens(combined_input) + 10
+        output_token = count_tokens(final_response)
 
-        # query_model.query_text_bot = final_response
-        # query_model.input_tokens = input_token
-        # query_model.output_tokens = output_token
-        # query_model.origin_url = origin_url
+        query_model.query_text_bot = final_response
+        query_model.input_tokens = input_token
+        query_model.output_tokens = output_token
+        query_model.origin_url = origin_url
 
-        # chatbot_stats.total_input_tokens += input_token
-        # chatbot_stats.total_output_tokens += output_token
-        # chatbot_stats.total_queries += 1
-        # chatbot_stats.last_query_time = datetime.datetime.now()
+        chatbot_stats.total_input_tokens += input_token
+        chatbot_stats.total_output_tokens += output_token
+        chatbot_stats.total_queries += 1
+        chatbot_stats.last_query_time = datetime.datetime.now()
 
    
-        # company = db.query(Company).filter(Company.id == user.id).first()
-        # if company:
-        #     company.input_tokens += input_token
-        #     company.output_tokens += output_token
+        company = db.query(Company).filter(Company.id == user.id).first()
+        if company:
+            company.input_tokens += input_token
+            company.output_tokens += output_token
         
-        # db.commit()
-        # logger.info("Successfully processed query for user ID: %s", user.id)
+        db.commit()
+        logger.info("Successfully processed query for user ID: %s", user.id)
 
-        return "Hello"
-    # except AttributeError as ae:
-    #     logger.error("AttributeError: %s", str(ae))
-    #     raise HTTPException(status_code=500, detail=f"An error occurred: {str(ae)}")
-    # except Exception as e:
-    #     logger.error("An unexpected error occurred: %s", str(e))
-    #     logger.error("Traceback: %s", traceback.format_exc())
-    #     db.rollback()  # Rollback on error
-    #     raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+        return final_response
+    except AttributeError as ae:
+        logger.error("AttributeError: %s", str(ae))
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(ae)}")
+    except Exception as e:
+        logger.error("An unexpected error occurred: %s", str(e))
+        logger.error("Traceback: %s", traceback.format_exc())
+        db.rollback()  # Rollback on error
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
